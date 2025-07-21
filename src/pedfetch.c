23b99a17eb83aa2891ac06579897e65b2f2473b6
Mehmet Mert Gunduz <merttgg@gmail.com>
2025-07-21 04:34:00 +0300

    add doxygen, main src file, logo and gitingore

---
+++ src/pedfetch.c
@@ -0,0 +1,16 @@
+/**
+ * @file pedfetch.c
+ * @author Mehmet Mert Gunduz (mmertgunduz@protonmail.com)
+ * @brief Main source file for pedfetch, a performance enhancing drugs fetching utility.
+ * @version 0.1
+ * @date 2025-07-21
+ * 
+ * @copyright Copyright (c) 2025 Mehmet Mert Gunduz
+*/
+
+#include <stdio.h>
+
+int main (int argc, char **argv) {
+
+    return 0;
+}
